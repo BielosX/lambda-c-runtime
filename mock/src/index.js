949419b3c1995b75c28966ba586442d10742c53b
Mateusz Biel <matbiel@gmail.com>
2024-03-15 09:41:40 +0100

    Use /2018-06-01 as path prefix

--- mock/src/index.js
+++ mock/src/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/runtime/invocation/next', (req, res) => {
+const router = express.Router()
+
+router.get('/runtime/invocation/next', (req, res) => {
     res.append('Lambda-Runtime-Aws-Request-Id', '8476a536-e9f4-11e8-9739-2dfe598c3fcd')
     res.append('Lambda-Runtime-Deadline-Ms', '1542409706888')
     res.append('Content-Type', 'application/json')
@@ -11,6 +13,8 @@ app.get('/runtime/invocation/next', (req, res) => {
         secondField: "value2"
     })
 })
+
+app.use('/2018-06-01', router)
 
 app.listen(port, () => {
     console.log(`Mock app listening on port ${port}`)
--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 
 int main(int argc, char** argv) {
     struct http_response response;
-    http_get("http://localhost:8080/runtime/invocation/next", &response);
-    http_get("http://localhost:8080/runtime/invocation/156cb537-e2d4-11e8-9b34-d36013741fb9/response", &response);
+    http_get("http://localhost:8080/2018-06-01/runtime/invocation/next", &response);
+    http_get("http://localhost:8080/2018-06-01/runtime/invocation/156cb537-e2d4-11e8-9b34-d36013741fb9/response", &response);
     http_get("http://google.com", &response);
     return 0;
 }
